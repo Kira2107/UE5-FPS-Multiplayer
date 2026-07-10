eaa0bc409c836e84d8828a55713db28583c89d49
Hritish Duvvur <hritish2001@gmail.com>
2026-07-10 18:50:10 -0400

    Added Weapon Class and BPs
    - Added a Weapon Class cpp
    - Made BPs for Pistol and Rifle, attached their meshes
    - Edited Quinn Skeleton weapon sockets to fit the guns properly

---
+++ Source/FPS/Private/Game/Weapons/Weapon.cpp
@@ -0,0 +1,43 @@
+﻿// Copyright by Hritish Duvvur
+
+
+#include "Game/Weapons/Weapon.h"
+
+#include "Components/SkeletalMeshComponent.h"
+
+
+// Sets default values
+AWeapon::AWeapon()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	
+	bReplicates = true;
+	bNetUseOwnerRelevancy = true; //If you can see the Owner, you can see and replicate the Weapon
+	
+	//First Person Mesh Settings
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh1P"));
+	Mesh1P -> VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered; //Arms animation happens only for the person who sees it, so basically only for the Player
+	Mesh1P -> bReceivesDecals = false;
+	Mesh1P -> CastShadow = false;
+	Mesh1P -> SetHiddenInGame(true);
+	SetRootComponent(Mesh1P);
+	
+	//Third Person Mesh Settings
+	Mesh3P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh3P"));
+	Mesh3P -> VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered; //Arms animation happens only for the person who sees it, so basically only for the Player
+	Mesh3P -> bReceivesDecals = false;
+	Mesh3P -> CastShadow = true;
+	Mesh3P -> SetupAttachment(Mesh1P);
+	Mesh3P -> SetHiddenInGame(true);
+}
+
+// Called when the game starts or when spawned
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
---
+++ Source/FPS/Public/Game/Weapons/Weapon.h
@@ -0,0 +1,35 @@
+﻿// Copyright by Hritish Duvvur
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UCLASS()
+class FPS_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AWeapon();
+	
+	//Getter for Meshes
+	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
+	USkeletalMeshComponent* GetMesh3P() const { return Mesh3P; }
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	
+	//Weapon Mesh: First Person View
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USkeletalMeshComponent> Mesh1P;
+
+	//Weapon Mesh: Third Person View
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USkeletalMeshComponent> Mesh3P;
+};
